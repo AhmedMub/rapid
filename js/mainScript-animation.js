8b853d24a79b599716ca9bef84984f452ae7615e
AhmedMub <mada.mub@gmail.com>
2020-09-18 12:10:29 +0300

    fast the animation

--- js/mainScript-animation.js
+++ js/mainScript-animation.js
@@ -1,97 +1,97 @@
 //first Section Animation
-document.addEventListener('DOMContentLoaded', () => {
-    anime({
-      targets: '.animated-intro-sec',
-      translateX: [-400, 0],
-      easing: 'easeOutExpo',
-      duration: 1500,
-      delay: 3000,
-      opacity: [0, 1]
-    });
-    anime({
-      targets: '.animated-introSec-btn',
-      translateY: [-5, 0],
-      easing: 'easeInOutQuad',
-      duration: 1000,
-      delay: 3000,
-      opacity: [0, 1]
-    });
-    anime({
-      targets: '.social-icnos ul li',
-      translateY: [-100, 0],
-      easing: 'easeOutExpo',
-      duration: 1000,
-      delay: function(el, i, l) {
-        return 3000 + i * 100;
-      },
-      endDelay: function(el, i, l) {
-        return (l - i) * 100;
-      },
-      opacity: [0, 1]
-    });
-    anime({
-      targets: '.navbarUl li',
-      translateY: [-100, 0],
-      easing: 'easeOutExpo',
-      duration: 2000,
-      delay: function(el, i, l) {
-        return 3000 + i * 100;
-      },
-      endDelay: function(el, i, l) {
-        return (l - i) * 100;
-      },
-      opacity: [0, 1]
-    });
-    anime({
-      targets: 'h1',
-      translateX: [-400, 0],
-      easing: 'easeOutExpo',
-      duration: 2000,
-      delay: function(el, i, l) {
-        return 3000 + i * 100;
-      },
-      endDelay: function(el, i, l) {
-        return (l - i) * 100;
-      },
-      opacity: [0, 1]
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  anime({
+    targets: ".animated-intro-sec",
+    translateX: [-400, 0],
+    easing: "easeOutExpo",
+    duration: 1500,
+    delay: 3000,
+    opacity: [0, 1],
   });
-  // typings info at intro section
+  anime({
+    targets: ".animated-introSec-btn",
+    translateY: [-5, 0],
+    easing: "easeInOutQuad",
+    duration: 1000,
+    delay: 3000,
+    opacity: [0, 1],
+  });
+  anime({
+    targets: ".social-icnos ul li",
+    translateY: [-100, 0],
+    easing: "easeOutExpo",
+    duration: 1000,
+    delay: function (el, i, l) {
+      return 3000 + i * 100;
+    },
+    endDelay: function (el, i, l) {
+      return (l - i) * 100;
+    },
+    opacity: [0, 1],
+  });
+  anime({
+    targets: ".navbarUl li",
+    translateY: [-100, 0],
+    easing: "easeOutExpo",
+    duration: 2000,
+    delay: function (el, i, l) {
+      return 3000 + i * 100;
+    },
+    endDelay: function (el, i, l) {
+      return (l - i) * 100;
+    },
+    opacity: [0, 1],
+  });
+  anime({
+    targets: "h1",
+    translateX: [-400, 0],
+    easing: "easeOutExpo",
+    duration: 2000,
+    delay: function (el, i, l) {
+      return 3000 + i * 100;
+    },
+    endDelay: function (el, i, l) {
+      return (l - i) * 100;
+    },
+    opacity: [0, 1],
+  });
+});
+// typings info at intro section
 let typed = new Typed(".typed-info", {
-    strings: ["business!", "finance!", "marketing!"],
-    typeSpeed: 60,
-    smartBackspace: true,
-    loop: true,
-    backSpeed: 30,
-    backDelay: 1600,
-  });
+  strings: ["business!", "finance!", "marketing!"],
+  typeSpeed: 60,
+  smartBackspace: true,
+  loop: true,
+  backSpeed: 30,
+  backDelay: 1600,
+});
 
 //scroll animation for All Section
 sal();
 
-//Animate ************************** 
+//Animate **************************
 
 //about us section
-const headerSec = document.querySelector('.about-observer');
+const headerSec = document.querySelector(".about-observer");
 let aboutOptions = {
-  rootMargin: '-100px 0px'
+  rootMargin: "-100px 0px",
 };
 let aboutObserver = new IntersectionObserver((entries, aboutObserver) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       anime({
-        targets: '.about-us .left-img',
+        targets: ".about-us .left-img",
         translateX: [-250, 0],
-        easing: 'easeOutCubic',
+        easing: "easeOutCubic",
         duration: 1000,
-        opacity: [0, 1]
+        opacity: [0, 1],
       });
       anime({
-        targets: '.about-us .right-info',
+        targets: ".about-us .right-info",
         translateX: [250, 0],
-        easing: 'easeOutCubic',
+        easing: "easeOutCubic",
         duration: 1000,
-        opacity: [0, 1]
+        opacity: [0, 1],
       });
       aboutObserver.unobserve(entry.target);
     }
@@ -100,26 +100,26 @@ let aboutObserver = new IntersectionObserver((entries, aboutObserver) => {
 
 aboutObserver.observe(headerSec);
 //why choose us section
-const chooserSec = document.querySelector('.why-choose-us');
+const chooserSec = document.querySelector(".why-choose-us");
 
 let chooseObserver = new IntersectionObserver((entries, chooseObserver) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       anime({
-        targets: '.why-choose-us .left',
+        targets: ".why-choose-us .left",
         translateX: [100, 0],
-        easing: 'easeOutCubic',
+        easing: "easeOutCubic",
         duration: 1000,
-        delay: 1000,
-        opacity: [0, 1]
+        delay: 500,
+        opacity: [0, 1],
       });
       anime({
-        targets: '.why-choose-us .right',
+        targets: ".why-choose-us .right",
         translateX: [-100, 0],
-        easing: 'easeOutCubic',
+        easing: "easeOutCubic",
         duration: 1000,
-        delay: 1000,
-        opacity: [0, 1]
+        delay: 500,
+        opacity: [0, 1],
       });
       chooseObserver.unobserve(entry.target);
     }
@@ -128,212 +128,218 @@ let chooseObserver = new IntersectionObserver((entries, chooseObserver) => {
 
 chooseObserver.observe(chooserSec);
 
-//features section
-const featuresSec = document.querySelector('.features-observer');
-
-let featuresObserver = new IntersectionObserver((entries, featuresObserver) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      anime({
-        targets: '.feature-inner .anime-feat1',
-        translateX: [100, 0],
-        easing: 'easeOutCubic',
-        duration: 1000,
-        delay: 1500,
-        opacity: [0, 1]
-      });
-      anime({
-        targets: '.feature-inner .anime-feat2',
-        translateX: [-100, 0],
-        easing: 'easeOutCubic',
-        duration: 1000,
-        delay: 1500,
-        opacity: [0, 1]
-      });
-      featuresObserver.unobserve(entry.target);
-    } 
-  });
-});
-
-featuresObserver.observe(featuresSec);
-//features section
-const featuresSec2 = document.querySelector('.features-observer2');
-
-let featuresSec2Observer = new IntersectionObserver((entries, featuresSec2Observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      anime({
-        targets: '.feature-inner .anime-feat3',
-        translateX: [100, 0],
-        easing: 'easeOutCubic',
-        duration: 1000,
-        delay: 1500,
-        opacity: [0, 1]
-      });
-      anime({
-        targets: '.feature-inner .anime-feat4',
-        translateX: [-100, 0],
-        easing: 'easeOutCubic',
-        duration: 1000,
-        delay: 1500,
-        opacity: [0, 1]
-      });
-      featuresSec2Observer.unobserve(entry.target);
-    }
-  });
-});
-
-featuresSec2Observer.observe(featuresSec2);
 //services section
-const servicesSec = document.querySelector('.h2Services');
-let boxOptions = { };
+const servicesSec = document.querySelector(".h2Services");
+let boxOptions = {};
 let boxObserver = new IntersectionObserver((entries, boxObserver) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       //animate boxes
       anime({
-        targets: '.animated-box',
+        targets: ".animated-box",
         translateY: [200, 0],
-        easing: 'easeInOutSine',
-        duration: 1500,
+        easing: "easeInOutSine",
+        duration: 1000,
         delay: (el, i) => {
           return 500 + 100 * i;
         },
-        opacity: [0, 1]
+        opacity: [0, 1],
       });
       //the icons
       let tl = anime({
-        targets: '.animatedIcon',
+        targets: ".animatedIcon",
         rotate: {
           value: 360,
-          duration: 3000,
-          easing: 'easeInOutSine',
-          delay: 1000
+          duration: 2000,
+          easing: "easeInOutSine",
+          delay: 800,
         },
       });
       boxObserver.unobserve(entry.target);
-    } 
+    }
   });
 }, boxOptions);
 
 boxObserver.observe(servicesSec);
+
+//features section
+const featuresSec = document.querySelector(".features-observer");
+
+let featuresObserver = new IntersectionObserver((entries, featuresObserver) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      anime({
+        targets: ".feature-inner .anime-feat1",
+        translateX: [100, 0],
+        easing: "easeOutCubic",
+        duration: 1000,
+        delay: 700,
+        opacity: [0, 1],
+      });
+      anime({
+        targets: ".feature-inner .anime-feat2",
+        translateX: [-100, 0],
+        easing: "easeOutCubic",
+        duration: 1000,
+        delay: 700,
+        opacity: [0, 1],
+      });
+      featuresObserver.unobserve(entry.target);
+    }
+  });
+});
+
+featuresObserver.observe(featuresSec);
+//features section
+const featuresSec2 = document.querySelector(".features-observer2");
+
+let featuresSec2Observer = new IntersectionObserver(
+  (entries, featuresSec2Observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        anime({
+          targets: ".feature-inner .anime-feat3",
+          translateX: [100, 0],
+          easing: "easeOutCubic",
+          duration: 1000,
+          delay: 700,
+          opacity: [0, 1],
+        });
+        anime({
+          targets: ".feature-inner .anime-feat4",
+          translateX: [-100, 0],
+          easing: "easeOutCubic",
+          duration: 1000,
+          delay: 700,
+          opacity: [0, 1],
+        });
+        featuresSec2Observer.unobserve(entry.target);
+      }
+    });
+  }
+);
+
+featuresSec2Observer.observe(featuresSec2);
+
 //portfolio section
-const portfolio = document.querySelector('.observe-port');
-let portOption = { };
+const portfolio = document.querySelector(".observe-port");
+let portOption = {};
 let portObserver = new IntersectionObserver((entries, portObserver) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       //animate boxes
       anime({
-        targets: '.port-categories li',
+        targets: ".port-categories li",
         translateY: [-100, 0],
-        easing: 'easeOutExpo',
+        easing: "easeOutExpo",
         duration: 1000,
-        delay: function(el, i, l) {
-          return 1000 + i * 100;
+        delay: function (el, i, l) {
+          return 800 + i * 100;
         },
-        endDelay: function(el, i, l) {
+        endDelay: function (el, i, l) {
           return (l - i) * 100;
         },
-        opacity: [0, 1]
+        opacity: [0, 1],
       });
       anime({
-        targets: '.images1 .hovereffect',
+        targets: ".images1 .hovereffect",
         translateY: [100, 0],
-        easing: 'easeOutExpo',
+        easing: "easeOutExpo",
         duration: 1300,
-        delay: function(el, i, l) {
-          return 1000 + i * 100;
+        delay: function (el, i, l) {
+          return 800 + i * 100;
         },
-        endDelay: function(el, i, l) {
+        endDelay: function (el, i, l) {
           return (l - i) * 100;
         },
-        opacity: [0, 1]
+        opacity: [0, 1],
       });
 
       portObserver.unobserve(entry.target);
-    } 
+    }
   });
 }, portOption);
 
 portObserver.observe(portfolio);
 
 //team section
-const TeamSec = document.querySelector('.team-observer');
+const TeamSec = document.querySelector(".team-observer");
 let teamSecObserver = new IntersectionObserver((entries, teamSecObserver) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       //animate boxes
       anime({
-        targets: '.team-content .person-info',
+        targets: ".team-content .person-info",
         translateX: [-100, 0],
-        easing: 'easeOutExpo',
+        easing: "easeOutExpo",
         duration: 1000,
-        delay: function(el, i, l) {
-          return 1500 + i * 100;
+        delay: function (el, i, l) {
+          return 800 + i * 100;
         },
-        endDelay: function(el, i, l) {
+        endDelay: function (el, i, l) {
           return (l - i) * 100;
         },
-        opacity: [0, 1]
+        opacity: [0, 1],
       });
 
       teamSecObserver.unobserve(entry.target);
-    } 
+    }
   });
 });
 
 teamSecObserver.observe(TeamSec);
 
 //pricing section
-const PricingSec = document.querySelector('.pricing-observer');
-let pricingSecObserver = new IntersectionObserver((entries, pricingSecObserver) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      //animate boxes
-      anime({
-        targets: '.prices div',
-        translateY: [100, 0],
-        easing: 'easeOutExpo',
-        duration: 1000,
-        delay: function(el, i, l) {
-          return 1500 + i * 100;
-        },
-        endDelay: function(el, i, l) {
-          return (l - i) * 100;
-        },
-        opacity: [0, 1]
-      });
+const PricingSec = document.querySelector(".pricing-observer");
+let pricingSecObserver = new IntersectionObserver(
+  (entries, pricingSecObserver) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        //animate boxes
+        anime({
+          targets: ".prices div",
+          translateY: [100, 0],
+          easing: "easeOutExpo",
+          duration: 1000,
+          delay: function (el, i, l) {
+            return 800 + i * 100;
+          },
+          endDelay: function (el, i, l) {
+            return (l - i) * 100;
+          },
+          opacity: [0, 1],
+        });
 
-      pricingSecObserver.unobserve(entry.target);
-    } 
-  });
-});
+        pricingSecObserver.unobserve(entry.target);
+      }
+    });
+  }
+);
 
 pricingSecObserver.observe(PricingSec);
 
 //Frequently Asked Questions section
-const frqSec = document.querySelector('.frq-observer');
+const frqSec = document.querySelector(".frq-observer");
 let frqSecObserver = new IntersectionObserver((entries, frqSecObserver) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       //animate boxes
       anime({
-        targets: '.QA div',
+        targets: ".QA div",
         translateY: [100, 0],
-        easing: 'easeOutExpo',
+        easing: "easeOutExpo",
         duration: 1000,
-        delay: function(el, i, l) {
-          return 1500 + i * 100;
+        delay: function (el, i, l) {
+          return 800 + i * 100;
         },
-        endDelay: function(el, i, l) {
+        endDelay: function (el, i, l) {
           return (l - i) * 100;
         },
-        opacity: [0, 1]
+        opacity: [0, 1],
       });
 
       frqSecObserver.unobserve(entry.target);
-    } 
+    }
   });
 });
 
--- js/mainScript.js
+++ js/mainScript.js
@@ -1,14 +1,16 @@
-//page on load 
+//page on load
 window.onload = () => {
-  setTimeout(function() {this.scrollTo(0, 0);},200);
-}
+  setTimeout(function () {
+    this.scrollTo(0, 0);
+  }, 200);
+};
 
 //spinner
-document.addEventListener('DOMContentLoaded', () => {
-  let spinner = document.querySelector('.spinner');
+document.addEventListener("DOMContentLoaded", () => {
+  let spinner = document.querySelector(".spinner");
   setTimeout(() => {
     spinner.remove();
-    document.body.classList.remove('body-hidden');
+    document.body.classList.remove("body-hidden");
   }, 3000);
 });
 
@@ -20,7 +22,6 @@ function headerHeight(param) {
   param.style.height = wind + "px";
 }
 headerHeight(header);
-
 
 //Navbar small screen
 const icons = document.getElementById("navIcons");
@@ -35,16 +36,15 @@ icons.onclick = () => {
   icons.lastElementChild.classList.toggle("disabled");
   document.body.classList.toggle("body-hidden");
 };
-navItem.forEach(el => {
-  el.addEventListener('click', () => {
-  overlay.classList.toggle("disabled");
-  ulNav.classList.toggle("enableUl");
-  icons.firstElementChild.classList.toggle("disabled");
-  icons.lastElementChild.classList.toggle("disabled");
-  document.body.classList.remove("body-hidden");
+navItem.forEach((el) => {
+  el.addEventListener("click", () => {
+    overlay.classList.toggle("disabled");
+    ulNav.classList.toggle("enableUl");
+    icons.firstElementChild.classList.toggle("disabled");
+    icons.lastElementChild.classList.toggle("disabled");
+    document.body.classList.remove("body-hidden");
   });
 });
-
 
 //navbar fixed on scroll
 const scrolledNav = document.querySelector(".menu");
@@ -62,7 +62,6 @@ let observer = new IntersectionObserver((entries, observer) => {
 }, options);
 
 observer.observe(intersecting);
-
 
 //count up numbers
 let secStatice = document.querySelector(".inner");
@@ -115,54 +114,47 @@ $(function () {
   var $obj = lc_lightbox("#lcl_elems_wrapper a");
 });
 
-// testimonials carousal 
-const slides = Array.from(document.querySelectorAll('.slides .slide'));
-const indicators = document.querySelector('.indicators');
+// testimonials carousal
+const slides = Array.from(document.querySelectorAll(".slides .slide"));
+const indicators = document.querySelector(".indicators");
 let activeSlide = 1;
 
-
-
-let theUl = document.createElement('ul');
-theUl.setAttribute('id', 'UlId');
+let theUl = document.createElement("ul");
+theUl.setAttribute("id", "UlId");
 indicators.appendChild(theUl);
 
-//add ul li indicators 
+//add ul li indicators
 for (let i = 1; i <= slides.length; i++) {
-
   //the li
-  let theLi = document.createElement('li');
+  let theLi = document.createElement("li");
   theUl.appendChild(theLi);
-  theLi.setAttribute('data-index', i);
+  theLi.setAttribute("data-index", i);
 }
 
-const createdLi = Array.from(document.querySelectorAll('#UlId li'));
+const createdLi = Array.from(document.querySelectorAll("#UlId li"));
 //Add active to each slide
 function AddActiveSlide() {
   activatedRemoved();
-  slides[activeSlide - 1].classList.add('item-activated');
-  createdLi[activeSlide - 1].classList.add('active-li');
+  slides[activeSlide - 1].classList.add("item-activated");
+  createdLi[activeSlide - 1].classList.add("active-li");
 }
 
-
-//remove activated 
+//remove activated
 function activatedRemoved() {
   slides.forEach((e) => {
-    e.classList.remove('item-activated');
+    e.classList.remove("item-activated");
   });
 
   createdLi.forEach((e) => {
-    e.classList.remove('active-li');
+    e.classList.remove("active-li");
   });
 }
 
 AddActiveSlide();
 
 for (let i = 0; i < createdLi.length; i++) {
-
-
   createdLi[i].onclick = () => {
-
-    activeSlide = parseInt(createdLi[i].getAttribute('data-index'));
+    activeSlide = parseInt(createdLi[i].getAttribute("data-index"));
     AddActiveSlide();
   };
 }
@@ -170,50 +162,41 @@ for (let i = 0; i < createdLi.length; i++) {
 let slideNumber = 1;
 
 setInterval(() => {
-
   let testing = parseInt(Math.ceil(Math.random() * slides.length));
   activeSlide = testing;
   AddActiveSlide();
-  
 }, 3000);
 
 //Frequently Questions fns
-const bulbs = document.querySelectorAll('.per-QA');
+const bulbs = document.querySelectorAll(".per-QA");
 
 //This code is for the wrapper(.per-QA) on clicking***********************
 bulbs.forEach((e) => {
   e.addEventListener("click", () => {
-
-    const answers  = e.lastElementChild;
+    const answers = e.lastElementChild;
     const heading = e.childNodes[3].lastElementChild;
 
-    e.classList.toggle('colordH4'); 
+    e.classList.toggle("colordH4");
 
-    if (e.classList.contains('colordH4')) {
-      answers.style.maxHeight = answers.scrollHeight + 'px';
-      heading.classList.remove('disabled');
-
+    if (e.classList.contains("colordH4")) {
+      answers.style.maxHeight = answers.scrollHeight + "px";
+      heading.classList.remove("disabled");
     } else {
       answers.style.maxHeight = 0;
-      heading.classList.add('disabled');
+      heading.classList.add("disabled");
     }
   });
 });
 
 //scroll top btn disabled
-const scrollTop = document.querySelector('.scroll-top'); 
+const scrollTop = document.querySelector(".scroll-top");
 window.onscroll = () => {
   if (window.pageYOffset >= 300) {
     scrollTop.style.opacity = "1";
   } else {
     scrollTop.style.opacity = "0";
   }
-}
+};
 scrollTop.onclick = () => {
   this.scrollTo(0, 0);
-}
-
- 
-
-
-
+};
